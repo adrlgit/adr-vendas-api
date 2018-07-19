e531c4631acbee8212d36ad3b9e74d4e4e20ac1a
André luis <luis.alms91@gmail.com>
2018-07-19 20:47:55 -0300

    Projeto Criado

---
+++ src/main/java/com/luis/vendas/AdrVendasApiApplication.java
@@ -0,0 +1,12 @@
+package com.luis.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdrVendasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdrVendasApiApplication.class, args);
+	}
+}
