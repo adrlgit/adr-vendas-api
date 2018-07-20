c40054357f543f66a9e162aa31490f67c6df21cf
André luis <luis.alms91@gmail.com>
2018-07-19 22:17:43 -0300

    Criação da Classe de Controle e ImportSQL + teste no Postman

---
+++ src/main/java/com/luis/vendas/resource/ClientesResource.java
@@ -0,0 +1,24 @@
+package com.luis.vendas.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luis.vendas.model.Cliente;
+import com.luis.vendas.repository.Clientes;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClientesResource {
+
+	@Autowired
+	private Clientes clientes;
+	
+	@GetMapping
+	public List<Cliente> listar(){
+		return clientes.findAll();
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into cliente (id, nome) values (1, 'Eletrônicos Eletro');
+insert into cliente (id, nome) values (2, 'Smart Compras');
