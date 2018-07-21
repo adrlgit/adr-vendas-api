f4f3a522bcd4e899671f131309d47d84b367fb41
André luis <luis.alms91@gmail.com>
2018-07-21 18:30:54 -0300

    Anotação @CrossOrigin(*)

--- src/main/java/com/luis/vendas/resource/VendasResource.java
+++ src/main/java/com/luis/vendas/resource/VendasResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.luis.vendas.model.Venda;
 import com.luis.vendas.repository.Vendas;
 import com.luis.vendas.service.VendaService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/vendas")
 public class VendasResource {
